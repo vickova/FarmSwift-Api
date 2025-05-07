b5df34b6f8151032b851359fae7d6063fee00e79
Olumide Victoria <olumideronke@gmail.com>
2025-05-08 00:01:37 +0100

    FarmSwiftLogo

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -102,7 +102,7 @@ export const initializePayment = async (req, res) => {
             customizations: {
                 title: "FarmSwift Payment",
                 description: "Payment for order",
-                logo: "https://yourlogo.com/logo.png"
+                logo: "https://imgur.com/a/6qoeQGb"
             }
         };
 
